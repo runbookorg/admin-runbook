23929fcec59baacfdf785f30ae415b8774f2ee53
Veera Reddy Obula <veerareddy.obula@gmail.com>
2019-04-21 08:21:08 -0700

    Validating Zeit-Now Auto deploy on push setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div>Welcome to Next.js! ...I am under serverless guidence</div>
+    return <div>Welcome to Next.js! ...I am under serverless guidence.. checking auto deploy</div>
 }
 
 export default Home
